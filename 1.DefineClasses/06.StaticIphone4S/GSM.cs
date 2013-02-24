40329d29d9a993bdc7ddcbc50c4bb1a6b621229b
Dobromir Petrov <luzterin@gmail.com>
2013-02-24 21:31:04 +0200

    StaticField iphone4s

---
+++ 1.DefineClasses/06.StaticIphone4S/GSM.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Text;
+
+namespace _06.StaticIphone4S
+{
+    class GSM
+    {
+        public Display Display = new Display();
+        public Battery Battery = new Battery("");
+        public Battery.BatteryType TypeOfBattery;
+
+        private string model;
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+
+        private decimal? price;
+
+        public decimal? Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Invalid Price!");
+                }
+                else
+                {
+                    this.price = value;
+                }
+            }
+        }
+
+        private string owner;
+
+        public string Owner
+        {
+            get
+            {
+                return owner;
+            }
+            set
+            {
+                this.owner = value;
+            }
+        }
+
+        private string manufacturer;
+
+        public string Manufacturer
+        {
+            get
+            {
+                return manufacturer;
+            }
+            set
+            {
+                manufacturer = value;
+            }
+        }
+
+        private static GSM iphone4S;
+
+        public static GSM IPhone4S
+        {
+            get { return iphone4S; }
+        }
+
+        static GSM()
+        {
+            iphone4S = new GSM("4S", "Apple");
+        }
+
+
+        public GSM(string model, string manufacturer)
+        {
+            this.Model = model;
+            this.Manufacturer = manufacturer;
+            this.price = null;
+            this.owner = null;
+        }
+
+        public GSM(string model, string manufacturer, decimal price) : this(model, manufacturer)
+        {
+            if (price < 0)
+            {
+                throw new ArgumentOutOfRangeException("Invalid value!");
+            }
+            else
+            {
+                this.price = price;
+            }
+            this.owner = null;
+        }
+
+        public GSM(string model, string manufacturer, decimal price, string owner) : this(model, manufacturer, price)
+        {
+            if (price < 0)
+            {
+                throw new ArgumentOutOfRangeException("Invalid value!");
+            }
+            else
+            {
+                this.price = price;
+            }
+            this.owner = owner;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append("GSM Model: " + Model);
+            str.Append(Environment.NewLine);
+            str.Append("GSM Manufacturer: " + Manufacturer);
+            str.Append(Environment.NewLine);
+            str.Append("GSM Price: " + Price + " лв.");
+            str.Append(Environment.NewLine);
+            str.Append(Environment.NewLine);
+            str.Append("Battery Model: " + Battery.Model);
+            str.Append(Environment.NewLine);
+            str.Append("Battery Type: " + Battery.TypeOfBattery);
+            str.Append(Environment.NewLine);
+            str.Append("Battery Idle Time: " + Battery.HoursIdle + " hours");
+            str.Append(Environment.NewLine);
+            str.Append("Battery Talk Hours: " + Battery.HoursTalk + " hours");
+            str.Append(Environment.NewLine);
+            str.Append(Environment.NewLine);
+            str.Append("Display Size: " + Display.Size + "\"");
+            str.Append(Environment.NewLine);
+            str.Append("Display Colors: " + Display.NumberOfColors + "M");
+            str.Append(Environment.NewLine);
+            str.Append("GSM Owner: " + Owner);
+            str.Append(Environment.NewLine);
+            return str.ToString();
+        }
+    }
+}
---
+++ 1.DefineClasses/06.StaticIphone4S/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using _06.StaticIphone4S;
+
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        GSM gsm1 = new GSM("Nokia", "Nokia Corp");
+
+        gsm1.Price = 1300;
+
+        gsm1.Battery.HoursIdle = 120;
+        gsm1.Battery.HoursTalk = 12;
+        gsm1.TypeOfBattery = Battery.BatteryType.NiCd;
+        gsm1.Battery.Model = "China";
+
+        gsm1.Display.NumberOfColors = 16;
+        gsm1.Display.Size = 4.5;
+
+        gsm1.Owner = "Pencho";
+        GSM gsm2 = GSM.IPhone4S;
+        
+        
+        Console.WriteLine(gsm1);
+
+        Console.WriteLine(gsm2);
+
+    }
+}
