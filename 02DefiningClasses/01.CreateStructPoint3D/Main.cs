2c85a11cb8c5f51eeead2fcf4640680d160290e2
Dobromir Petrov <luzterin@gmail.com>
2013-03-08 08:26:01 +0200

    small fixs

--- 02DefiningClasses/01.CreateStructPoint3D/Main.cs
+++ 02DefiningClasses/01.CreateStructPoint3D/Main.cs
@@ -11,27 +11,31 @@ namespace _01.CreateStructPoint3D
         static void Main(string[] args)
         {
             Point3d pointOne = new Point3d();
-            Point3d pointTwo = new Point3d();
 
             pointOne.PointX = 0.5;
             pointOne.PointY = 0.5;
             pointOne.PointZ = 0.5;
             Console.WriteLine(pointOne);
-
-            pointTwo.PointX = 1.5;
-            pointTwo.PointY = 1.5;
-            pointTwo.PointZ = 1.5;
+            Console.WriteLine(Point3d.ZeroPosition);
+            Point3d pointTwo = new Point3d(1.5, 1.5, 1.5);
+            
             Console.WriteLine(pointTwo);
+            Console.WriteLine("Distance between");
             Console.WriteLine(Distance.DistanceBetween(pointOne, pointTwo));
+
             Path path = new Path();
             path.Add(pointOne);
             path.Add(pointTwo);
             PathStorage.SavePath(path, @"../../Points3d.txt");
-
-            Path newLoadedPath = PathStorage.LoadPath(@"../../Points3d.txt");
-
+            path.Clear();
             Console.WriteLine();
-            foreach (var item in newLoadedPath.Paths)
+            Console.WriteLine("Path is saved and then cleared");
+
+            path = PathStorage.LoadPath(@"../../Points3d.txt");
+            Console.WriteLine();
+            Console.WriteLine("After reloading again from file");
+            Console.WriteLine();
+            foreach (var item in path.Paths)
             {
                 Console.WriteLine(item);
             }
